0509d586860b816ce41401c28a72e967184e2700
Vatsal <vatsll@yahoo.com>
2023-06-12 12:02:33 +0530

    side nav smooth

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { MovieService } from '../movie.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
   movies: any[] = [];
@@ -22,6 +22,7 @@ export class HomeComponent implements OnInit {
   isUpcomingMovieHover: boolean = false;
   hoveredImageIndex: number = 0;
   hoveredImageData: any;
+  
   constructor(private movieService: MovieService) { }
 
   ngOnInit() {
@@ -134,4 +135,5 @@ export class HomeComponent implements OnInit {
     this.hoveredImageData = hoveredData;
     this.isUpcomingMovieHover = true;
   }
+
 }
